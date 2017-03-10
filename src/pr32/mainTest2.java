99db031fb4c8e17c2586152d2b8c64d7a6d08be8
admin <tdene.ivanov@yandex.ru>
2017-03-10 12:09:09 +0300

    change1

--- src/pr32/mainTest2.java
+++ src/pr32/mainTest2.java
@@ -1,7 +1,30 @@
 package pr32;
 
-/**
- * Created by admin on 10.03.2017.
- */
+
+import java.io.*;
+
 public class mainTest2 {
+    public static void main(String[] arg) throws IOException {
+
+        /*try(InputStream is=new FileInputStream("")){
+            byte[] buf=new byte[1024];
+            int n=is.read(buf);
+            while (true){
+                n=is.read(buf);
+            }
+
+        }*/
+        try (InputStream is = new BufferedInputStream(new FileInputStream("C:\\Archi\\ReadMe.txt"));
+             OutputStream os = new BufferedOutputStream(new FileOutputStream("random.txt"))) {
+            while (true)
+
+            {
+                int c = is.read();
+                if (c < 0) break;
+                os.write(c);
+            }
+        }
+    }
 }
+
+
